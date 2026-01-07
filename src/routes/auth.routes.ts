adf5973c53bce6831050f32b02e4691908b4b807
Abhishek Mahato <abhishekmahato2002@gmail.com>
2026-01-07 20:08:31 +0530

    send email sample

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,23 @@
 import { Router } from "express";
 import { getAllUser, getUserById, login, signUp } from "../controllers";
+import { transporter } from "../lib";
 
 const router = Router();
 
-router.post("/signup", signUp); 
+router.post("/signup", signUp);
 
-router.post('/login',login);
+router.post("/login", login);
 
-router.get('/users', getAllUser)
-router.get('/users/:id', getUserById)
+router.get("/users", getAllUser);
+router.get("/users/:id", getUserById);
 
+router.post("/test-send-email", async (req, res) => {
+  await transporter.sendMail({
+    to: "foreverabhi@mailinator.com",
+    subject: `Invitation to join `,
+    html: `<h3>You have been invited to join</h3>`,
+  });
+  res.send("Email sent!");
+});
 
 export default router;
